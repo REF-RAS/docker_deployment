8cc3034ee1b3642c03769312bc733d3a167c5e43
Das <dasun.gunasinghe@qut.edu.au>
2024-02-13 17:50:34 +1000

    Initial test updates - needs more work to get a first iteration working

--- images/base/armer/assets/entrypoint_setup.sh
+++ images/base/armer/assets/entrypoint_setup.sh
@@ -4,7 +4,7 @@ echo "> Setting up ROS"
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
 echo "> Setting up catkin workspace"
-source "$ARMER_WS/devel/setup.bash"
+source "$ROBOT_WS/devel/setup.bash"
 
 # Run CMD from Dockerfile or the overriding command from docker compose yaml file
 echo "> Running $@"
